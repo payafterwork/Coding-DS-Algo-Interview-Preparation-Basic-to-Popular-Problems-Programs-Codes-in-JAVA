5f79ea6c98c20b47bcf03ae77eec95b840e5e9cd
Harshit Batra <payafterwork.in@gmail.com>
2019-06-17 17:09:33 +0530

    Basic + LargestOfThree + isPrime

---
+++ Basics.java
@@ -0,0 +1,8 @@
+
+public class Basi {
+
+	public static void main(String[] args) {
+		  System.out.println("Hello");
+
+	}
+}
---
+++ isPrime.java
@@ -0,0 +1,40 @@
+
+public class isPrime {
+    public static void main(String[] args) {
+    	int n=7;
+    	int counter=2; // Checking from 2 (1 divides every number anyway)
+
+//BEST
+    	while(counter*counter<=n) { // till square root of 'n' because after that root begins to repeat
+    		if (n%counter==0) {
+    			System.out.println("Not prime");
+    		    return;
+    		}
+    		counter++;
+    	}    	
+
+      System.out.println("Prime");
+    	
+    	
+//Better
+//    	while(counter<=n/2) { // till n/2 because no root after half anyway 
+//    		if (n%counter==0) {
+//    			System.out.println("Not prime");
+//    		    return;
+//    		}
+//    		counter++;
+//    	}
+
+//    	System.out.println("Prime");
+    	
+    	
+// SIMPLEST   	while(counter<=n-1) { // till n-1 (because every no. divides itself)
+//    		if (n%counter==0) {
+//    			System.out.println("Not prime");
+//    		    return;
+//    		}
+//    		counter++;
+//    	}
+//    	System.out.println("Prime");
+    }
+}
