102316ff096ac5ab137f02c1ffd2f28bfbcb698e
Harshit Batra <payafterwork.in@gmail.com>
2019-08-10 12:33:34 +0530

    Reverting Back to Old Structure

---
+++ AllPrime.java
@@ -0,0 +1,24 @@
+
+public class AllPrime {
+    public static void main(String[] args) {
+    	int n=7;
+    	int counter=2; // Checking from 2 (1 divides every number anyway)
+        int checknum=2; // 2 se prime check kro until the number n 
+    while(checknum<=n) {
+    	while(counter*counter<=checknum) //2. 
+    		{ // till square root of 'n' because after that root begins to repeat
+    		if (checknum%counter==0) {
+    		    break; //1.
+    		}
+    		counter++;
+    	}   // 2 exist points for this loop:
+    	 // 1. Not Prime Case: Where counter was able to divide checksum and break;  
+    	 // 2. Prime Case: When 2. condition ran out as counter value increased, failing while condition, but as counter value was incremented counter*counter >n can be used to detect this way of exiting of loop.
+    	 if(counter*counter>checknum) {
+    		 System.out.println(checknum);
+           }
+   checknum++;  	
+   }
+ }
+}    
+    
---
+++ LargestOfThree.java
@@ -0,0 +1,31 @@
+
+public class LargestOfThree {
+     
+	
+	public static void main(String[] args) {
+	 int  a =10; 
+     int b=10;
+     int c=10;
+     
+     	if(a>=b && a>=c) {
+     		System.out.println(a + " is largest");
+     	}else if(b>=a && b>=c) {
+     		System.out.println(b + " is largest");
+     	}else {
+    	  System.out.println(c + " is largest");
+     	}
+     	
+//       	
+//     	
+//     	if(a>=b && a>=c) {
+// 		System.out.println(a + " is largest");
+// 	}else{
+// 		if(b>=a && b>=c) {
+// 			System.out.println(b + " is largest");
+// 		}else {
+// 			System.out.println(c + " is largest");
+// 		}
+// 	}		
+//}
+	}
+}
