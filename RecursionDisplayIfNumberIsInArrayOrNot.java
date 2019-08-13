743fbe78180910a5baa2485c1c4d0954a6c253dc
Harshit Batra <payafterwork.in@gmail.com>
2019-08-13 15:21:19 +0530

    RecursionDisplayIfNumberIsInArrayOrNot

---
+++ RecursionDisplayIfNumberIsInArrayOrNot.java
@@ -0,0 +1,21 @@
+
+public class RecursionDisplayIfNumberIsInArrayOrNot {
+   
+	public static void main(String[] args) {
+		int[] arr =  {5,3,9,12};
+		System.out.print(InItOrNot(arr,10,0));
+	}
+	
+	public static boolean InItOrNot(int[] arr,int data,int vindex) {
+       if(vindex==arr.length) {
+    	   return false; // Reached end of array, couldn't find it.
+       }
+		if(arr[vindex]==data) {
+	    	return true;
+	    }else {
+	    	boolean fn = InItOrNot(arr, data, vindex+1);
+	    	return fn;
+	    }	
+	}
+	
+}
